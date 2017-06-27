1d3c662ceb47c4da9a5ba93615788bbf1c4d3175
Demos <dtsellos03@gmail.com>
2017-06-27 03:41:24 +0000

    Added dependencies

--- api-call-tests.js
+++ api-call-tests.js
@@ -3,9 +3,13 @@ const api_key = require('./api_key.js')
 
 var request = require('request');
 request('https://www.googleapis.com/youtube/v3/commentThreads?key='+api_key+'&textFormat=plainText&part=snippet&videoId=vk0F8dHo3wU&maxResults=100', function (error, response, body) {
-
+      var totalcomment = []
       var parsed = JSON.parse(body)
       console.log(parsed.kind)
+      parsed.items.forEach(function(element){
+            totalcomment.push(element.snippet.topLevelComment.snippet.textOriginal);
+      })
+      console.log(totalcomment)
 
 });
 
