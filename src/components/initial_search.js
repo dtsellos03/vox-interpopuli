0141506c3887c0c5dc662b2b345fe39f9a16a5f4
Demos <dtsellos03@gmail.com>
2017-07-09 22:08:43 +0000

    Added component to display initial search message

---
+++ src/components/initial_search.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+
+const InitialSearch = (commentArray) => {
+  
+
+  
+    return (
+      <div>
+
+  <h1>Please enter a search term to see what YouTube has to say about it.</h1>
+  
+  
+    </div>
+  )
+
+}
+
+export default InitialSearch;
