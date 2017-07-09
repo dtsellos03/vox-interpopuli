0909af221b8b412eb6dbd9e4dffba4c3afc8505d
Demos <dtsellos03@gmail.com>
2017-07-09 22:09:43 +0000

    CORS headers

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -28,6 +28,12 @@ module.exports = {
   devServer: {
     stats: { colors: true },
     disableHostCheck: true,
-    contentBase: './dist'
+    contentBase: './dist',
+     headers: {
+    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, x-id, Content-Length, X-Requested-With",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
+}
   }
 };
