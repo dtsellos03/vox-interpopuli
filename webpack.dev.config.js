86135c05f798411505671117e0517658b22cd8fa
Demos <dtsellos03@gmail.com>
2017-07-19 20:18:08 +0000

    Removed unused plugins

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -24,4 +24,32 @@ module.exports = {
   },
   devtool: 'cheap-eval-source-map',
 
+};var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.js' // Your appʼs entry point
+  ],
+  output: { 
+    path: './public/js/app',
+    filename: 'bundle.js',
+    //publicPath: pathPublicCDN
+  },
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel?presets[]=react,presets[]=es2015']
+      }
+    ]
+  },
+  devtool: 'cheap-eval-source-map',
+
 };
