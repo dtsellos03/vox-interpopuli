7f1c93c4e8d3579ea13a5ae97fa9ad9d35011b86
Demos <dtsellos03@gmail.com>
2017-07-19 00:16:56 +0000

    Added component that displays autogenerated text

---
+++ src/components/paragraph_text.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+
+const ParagraphText = (paragraph) => {
+  
+ 
+    return (
+<div>
+<h3 className="header">Text generated from popular comments</h3>
+<p>{paragraph}</p>
+</div>
+      )
+
+
+}
+
+export default ParagraphText;
