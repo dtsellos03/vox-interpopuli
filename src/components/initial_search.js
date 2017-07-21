26e05412a0d164fe7f8699e94481b44595a6a2a7
Demos <dtsellos03@gmail.com>
2017-07-21 22:18:43 +0000

    Added landing taxt

--- src/components/initial_search.js
+++ src/components/initial_search.js
@@ -8,10 +8,11 @@ const InitialSearch = (commentArray) => {
 
   
     return (
-      <div>
+      <div className="ui very padded text">
 
   <h1>Please enter a search term to see what YouTube has to say about it.</h1>
-  
+  <h3>This application retrieves the fifty most relevant videos 
+  and analyzes one hundred comments from each video. The most commonly used phrases, adjectives, nouns, etc are then presented in a tag cloud.</h3>
   
     </div>
   )
